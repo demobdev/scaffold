7f7f66f0baed342313b5018426a4e6f87de7ad16
Emmanuelbreezy <emmabreezyumeh@gmail.com>
2025-12-15 10:12:39 +0100

    🎉Begin Project

---
+++ app/api/project/[id]/route.ts
@@ -0,0 +1,45 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  try {
+    const { id } = await params;
+    const session = await getKindeServerSession();
+    const user = await session.getUser();
+
+    if (!user) throw new Error("Unauthorized");
+
+    const project = await prisma.project.findFirst({
+      where: {
+        userId: user.id,
+        id: id,
+      },
+      include: {
+        frames: true,
+      },
+    });
+
+    if (!project) {
+      return NextResponse.json(
+        {
+          error: "Project not found",
+        },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(project);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      {
+        error: "Fail to fetch project",
+      },
+      { status: 500 }
+    );
+  }
+}
---
+++ context/canvas-context.tsx
@@ -0,0 +1,106 @@
+import { THEME_LIST, ThemeType } from "@/lib/themes";
+import { FrameType } from "@/types/project";
+import {
+  createContext,
+  ReactNode,
+  useCallback,
+  useContext,
+  useState,
+} from "react";
+
+export type LoadingStatusType =
+  | "idle"
+  | "running"
+  | "analyzing"
+  | "generating"
+  | "completed";
+
+interface CanvasContextType {
+  theme?: ThemeType;
+  setTheme: (id: string) => void;
+  themes: ThemeType[];
+
+  frames: FrameType[];
+  setFrames: (frames: FrameType[]) => void;
+  updateFrame: (id: string, data: Partial<FrameType>) => void;
+  addFrame: (frame: FrameType) => void;
+
+  selectedFrameId: string | null;
+  selectedFrame: FrameType | null;
+  setSelectedFrameId: (id: string | null) => void;
+
+  loadingStatus: LoadingStatusType | null;
+}
+
+const CanvasContext = createContext<CanvasContextType | undefined>(undefined);
+
+export const CanvasProvider = ({
+  children,
+  initialFrames,
+  initialThemeId,
+  hasInitialData,
+  projectId,
+}: {
+  children: ReactNode;
+  initialFrames: FrameType[];
+  initialThemeId?: string;
+  hasInitialData: boolean;
+  projectId: string | null;
+}) => {
+  const [themeId, setThemeId] = useState<string>(
+    initialThemeId || THEME_LIST[0].id
+  );
+
+  const [frames, setFrames] = useState<FrameType[]>(initialFrames);
+  const [selectedFrameId, setSelectedFrameId] = useState<string | null>(null);
+
+  const [loadingStatus, setLoadingStatus] = useState<LoadingStatusType | null>(
+    hasInitialData ? "idle" : null
+  );
+
+  const theme = THEME_LIST.find((t) => t.id === themeId);
+  const selectedFrame =
+    selectedFrameId && frames.length !== 0
+      ? frames.find((f) => f.id === selectedFrameId) || null
+      : null;
+
+  //Update the LoadingState Inngest Realtime event
+
+  const addFrame = useCallback((frame: FrameType) => {
+    setFrames((prev) => [...prev, frame]);
+  }, []);
+
+  const updateFrame = useCallback((id: string, data: Partial<FrameType>) => {
+    setFrames((prev) => {
+      return prev.map((frame) =>
+        frame.id === id ? { ...frame, ...data } : frame
+      );
+    });
+  }, []);
+
+  return (
+    <CanvasContext.Provider
+      value={{
+        theme,
+        setTheme: setThemeId,
+        themes: THEME_LIST,
+        frames,
+        setFrames,
+        selectedFrameId,
+        selectedFrame,
+        setSelectedFrameId,
+        updateFrame,
+        addFrame,
+        loadingStatus,
+      }}
+    >
+      {children}
+    </CanvasContext.Provider>
+  );
+};
+
+export const useCanvas = () => {
+  const ctx = useContext(CanvasContext);
+  if (!ctx) throw new Error("useCanvas must be used inside CanvasProvider");
+  return ctx;
+};
